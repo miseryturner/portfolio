a2db703c2467e58df335166e224a1bd9356dfa07
miseryturner <mmizari2019@mail.ru>
2024-02-25 21:20:07 +0300

    optimize project request

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,9 +9,12 @@ use App\Models\Info;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects_full = Project::where('is_full', '1')->get();
-        $projects_tasks = Project::where('is_full', '0')->get();
+        $projects = Project::all();
+        $projects_full = $projects->where('is_full', 1);
+        $projects_tasks = $projects->where('is_full', 0); 
+
 	    $info = Info::all();
+        
         return view('welcome', compact('projects_full', 'projects_tasks', 'info'));
     }
 }
